6a242d840cb1c1673878729968f7cf03c327df3d
azatmagdeev <48885231+azatmagdeev@users.noreply.github.com>
2020-03-19 23:55:23 +0200

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,5 +1,9 @@
 <?php
-$connection = mysqli_connect('test.ru','root','','test_db');
+$connection = mysqli_connect(
+    'zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    'm9wt7vvlnlfh9vkd',
+    'b6ox6ai0q5g0kurs',
+    'q33820pojzrriioz');
 if ($connection == false) {
     echo 'false';
     echo mysqli_connect_error();
