088235fb6f47949cae2d68bcaefbc982920b6d4c
azatmagdeev <48885231+azatmagdeev@users.noreply.github.com>
2020-03-21 12:08:27 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,9 @@
 $config = array(
     'title' => 'Книжный клуб',
     'db' => array(
-        'server' => 'test.ru',
-        'username' =>'root',
-        'password'=>'',
-        'name'=>'test_db'
+        'server' => 'zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+        'username' => 'm9wt7vvlnlfh9vkd',
+        'password'=> 'b6ox6ai0q5g0kurs'',
+        'name'=> 'q33820pojzrriioz'
     )
 );
