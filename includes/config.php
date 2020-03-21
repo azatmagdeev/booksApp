d18131447f8daf2f46b0aab092994fa3bfdad1e2
azatmagdeev <48885231+azatmagdeev@users.noreply.github.com>
2020-03-21 18:33:25 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ $config = array(
     'db' => array(
         'server' => 'zpj83vpaccjer3ah.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
         'username' => 'm9wt7vvlnlfh9vkd',
-        'password'=> 'b6ox6ai0q5g0kurs'',
+        'password'=> 'b6ox6ai0q5g0kurs',
         'name'=> 'q33820pojzrriioz'
     )
 );
